72ebc99166ed4665451a90d999f3df79f56c6ae4
NissrineH <48525832+NissrineH@users.noreply.github.com>
2022-09-05 17:17:27 +0100

    Update gui.py

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,14 +1,12 @@
 #Packages
 import streamlit as st
-from vina import Vina
-
-#from pymol import cmd
-#import py3Dmol
+from pymol import cmd
+import py3Dmol
 import prolif as plf
 import MDAnalysis as mda
 from MDAnalysis.coordinates import PDB
-#from rdkit import Chem
-#from rdkit.Chem import AllChem, Draw
+from rdkit import Chem
+from rdkit.Chem import AllChem, Draw
 from openbabel import pybel
 from vina import Vina
 from meeko import MoleculePreparation
